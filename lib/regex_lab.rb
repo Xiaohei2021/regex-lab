1aabae0685dddb7d5fd73753d1000de813aa0c41
Leon <hualiang.tian@gmail.com>
2021-03-12 18:17:20 -0500

    commit tree afeac54d0d97b704e109c5be53bcd1e16a46ff07

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,43 @@
-def starts_with_a_vowel?(word)
+require 'pry'
+# def starts_with_a_vowel?(word)
+#     word.match? (/^[aeiou|AEIOU]/) do |words|
+#         return true if words == /^[aeiou|AEIOU]/
+#     end
+# end
 
+def starts_with_a_vowel?(word)
+    word.match(/^[aeiou|AEIOU]/)? true : false
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
+   
+   words_string = []
+    text.scan(/un\w+ing/) do |texts|
+        words_string << texts
+     end
+      words_string
+    end
 
 def words_five_letters_long(text)
 
-end
+    words_string = []
+    text.scan(/\b[a-z]{5}\b/) do |texts|
+        words_string << texts
+     end
+      words_string
+    end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+    def first_word_capitalized_and_ends_with_punctuation?(text)
+        text.match(/^[A-Z].+\.$/)? true :false
+    end
 
 def valid_phone_number?(phone)
+    phone.match(/^(\d{3}|\W....)(\s\d{3}|\d{3})(.\d{4}|\d{4})\b/)? true :false
 
+    # phone.match(/^[(]?\d{3}[)]?[\s]?\d{3}[\s-]?\d{4}$/)? true :false
+    
+    
+    # phone.match(/^(\d{3}|\W....)+(\d{3}|\s)\d{4}/)? true :false
+    
 end
